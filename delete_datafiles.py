78c45583e0c878ee1b57f34d81ee719d37c1f866
Stuart Pullinger <stuartpullinger@gmail.com>
2017-10-17 10:47:50 +0100

    Create command line options

--- delete_datafiles.py
+++ delete_datafiles.py
@@ -4,11 +4,12 @@
 import requests #use requests directly or use python-icat?
 import sys
 import logging
+import argparse
 
 # create global vars: host, session
 
-def get_session(mnemonic, username, password):
-    """Takes a username, password and mnemonic, logs into ICAT and returns a session ID"""
+def get_session(auth_mechanism, username, password):
+    """Takes a username, password and authentication mechanism, logs into ICAT and returns a session ID"""
     pass
 
 def get_datafile_id(location):
@@ -31,7 +32,47 @@ def process_location_file(location_file):
 
 def main():
     # parse commandline options
-    # set up logging
+    arg_parser = argparse.ArgumentParser(description="Takes a file containing one file path per line and delete the corresponding DataFile entities from ICAT")
+    arg_parser.add_argument('-f',
+                            '--locations-file',
+                            required=True,
+                            help="A file containing files to delete from ICAT, one file per line",
+                            metavar="<locations-file>",
+                            dest="locations_file")
+    arg_parser.add_argument('-i',
+                            '--icat-host',
+                            required=True,
+                            help="The ICAT host and port eg. 'http://example.com:8080/'",
+                            metavar="<http[s]://host:port>",
+                            dest="icat_host")
+    arg_parser.add_argument('-u',
+                            '--user',
+                            required=True,
+                            help="The username to use to connect to ICAT",
+                            metavar="<username>",
+                            dest="user")
+    arg_parser.add_argument('-m',
+                            '--mechanism',
+                            required=True,
+                            help="The authentication mechanism to use to connect to ICAT eg. simple|db|ldap",
+                            metavar="<authentication-mechanism>",
+                            dest="mechanism")
+    arg_parser.add_argument('-b',
+                            '--batch-size',
+                            default=200,
+                            help="The number of files to delete in a single call to ICAT",
+                            metavar="<batch-size>",
+                            dest="batch_size")
+    arg_parser.add_argument('-l',
+                            '--log-level',
+                            default='WARNING',
+                            choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG'],
+                            help="The logging level",
+                            metavar="<log-level>",
+                            dest="log_level")
+    args = arg_parser.parse_args()
+    # set up logging - from commandline option
+    logging.setLevel(logging[args.log_leve])
     # ask user for password
     # try opening file of locations
     # try creating a session - how long will the session last?
