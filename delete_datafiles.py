2e60bb0426dec3b37c52bde7f9c5468875b45731
Stuart Pullinger <stuartpullinger@gmail.com>
2017-10-27 15:19:21 +0100

    Added retries if icat session expires and more logging

--- delete_datafiles.py
+++ delete_datafiles.py
@@ -9,6 +9,13 @@ import argparse
 import itertools
 import getpass
 
+def log_response(response):
+    logging.debug("URL: %s", str(response.url))
+    logging.debug("Status Code: %s", str(response.status_code))
+    logging.debug("Response: %s", response.text if response.text else 'Empty')
+    logging.debug("Json: %s", response.json() if response.json() else 'Empty')
+
+
 def get_session(auth_mechanism, username, password, host):
     """Takes a username, password and authentication mechanism, logs into ICAT
     and returns a session ID"""
@@ -31,12 +38,10 @@ def get_session(auth_mechanism, username, password, host):
         return response.json()['sessionId']
     else:
         logging.critical("Failed to get a session ID. Exiting.")
-        logging.debug("URL: %s", str(response.url))
-        logging.debug("Status Code: %s", str(response.status_code))
-        logging.debug("Response: %s", response.text if response.text else 'Empty')
+        log_response(response)
         raise RuntimeError()
 
-def get_datafile_id(location, host, session):
+def get_datafile_id(location, host, session, connection_pool, auth):
     """Takes a file path location as a string and returns the corresponding
     DataFile ID from ICAT"""
 
@@ -46,22 +51,46 @@ def get_datafile_id(location, host, session):
     jpql_url = host + "/icat/entityManager/"
     query_data = (('sessionId', session), ('query', jpql_query))
 
-    # TODO Test for expired session. If expired, get new session and try again
-    response = requests.get(jpql_url, params=query_data)
+    retries = 3
+    while retries > 0:
+        response = connection_pool.get(jpql_url, params=query_data)
 
-    # If there are no results, ICAT still returns 200 OK and an empty response
-    # Therefore, we cannot check the response code
-    try:
-        return response.json()[0]
-    except IndexError as e:
-        logging.warning("Failed to get Datafile ID for: %s. Skipping.", location)
-        logging.debug("URL: %s", str(response.url))
-        logging.debug("Status Code: %s", str(response.status_code))
-        logging.debug("Response: %s", response.text if response.text else 'Empty')
-        logging.debug("Json: %s", response.json() if response.json() else 'Empty')
-        return None
+        if response.ok:
+            # If there are no results, ICAT still returns 200 OK and an empty response
+            try:
+                dfid = response.json()[0]
+                logging.debug("Got Datafile ID: %s for location: %s", str(dfid),
+                        location)
+                return dfid
+            except IndexError as e:
+                # Response is OK but JSON is empty
+                logging.warning("Failed to get Datafile ID for: %s. Skipping.", location)
+                log_response(response)
+                return None
+        else:
+            # Response is not OK
+            # Test for expired session. If expired, get new session and try again
+            try:
+                failed_session = "Unable to find user by sessionid" in response.json()['message']
+            except Exception as e:
+                logging.critical("Failed to get Datafile ID for unknown reason. Exiting")
+                log_response(response)
+                raise e
+            else:
+                if failed_session:
+                    logging.warning("Session ID not found. Retrying")
+                    session = get_session(auth[0], auth[1], auth[2], host)
+                    retries -= 1
+                    continue
+                else:
+                    logging.critical("Failed to get Datafile ID for unknown reason. See message in JSON output. Exiting")
+                    log_response(response)
+                    raise RuntimeError
+    logging.critical("Failed to get Datafile ID for unknown reason after retrying. Exiting")
+    log_response(response)
+    raise RuntimeError
 
-def delete_datafiles(datafiles, host, session):
+def delete_datafiles(datafiles, host, session, connection_pool, auth):
     """Takes a list of DataFile IDs and deletes them from ICAT"""
 
     logging.info("Attempting to delete datafile ids: %s", str(datafiles))
@@ -70,27 +99,46 @@ def delete_datafiles(datafiles, host, session):
     # '[ {"Datafile": {"id": 9933}}, {"Datafile": {"id": 3316}} ]'
     datafiles_as_json = (''.join( ( '{"Datafile": {"id": ', str(x), '}}') ) for x in
             datafiles)
-    # TODO make this an iterator - it currently constructs a string
+    # TODO make this an iterator - it currently constructs a string which is
+    # then discarded
     datafiles_with_commas = ', '.join(datafiles_as_json)
     datafiles_as_json_list = ''.join(('[ ', datafiles_with_commas, ' ]'))
     delete_url = host + "/icat/entityManager/"
 
-    # TODO Test for expired session. If expired, get new session and try again
-    response = requests.delete(delete_url, params= (('sessionId', session),
-        ('entities', datafiles_as_json_list)))
+    retries = 3
+    while retries > 0:
+        response = connection_pool.delete(delete_url, params= (('sessionId', session),
+            ('entities', datafiles_as_json_list)))
 
-    if response.ok:
-        return
-    else:
-        logging.warning("Failed to delete datafiles: %s. Skipping.", str(datafiles))
-        logging.debug("URL: %s", str(response.url))
-        logging.debug("Status Code: %s", str(response.status_code))
-        logging.debug("Response: %s", response.text if response.text else 'Empty')
-        logging.debug("Json: %s", response.json() if response.json() else 'Empty')
-        return
+        if response.ok:
+            return
+        else:
+            # Response is not OK
+            # Test for expired session. If expired, get new session and try again
+            try:
+                failed_session = "Unable to find user by sessionid" in response.json()['message']
+                #failed_session = response.json()['message'].find('Unable to find user by sessionid') != -1
+            except Exception as e:
+                logging.debug("No 'message' section in JSON response")
+                logging.warning("Failed to delete datafiles: %s. Skipping.", str(datafiles))
+                log_response(response)
+                return
+            else:
+                if failed_session:
+                    logging.warning("Session ID not found. Retrying")
+                    session = get_session(auth[0], auth[1], auth[2], host)
+                    retries -= 1
+                    continue
+                else:
+                    logging.warning("Failed to get Datafile ID for unknown reason. See message in JSON output. Skipping")
+                    log_response(response)
+                    return
+    logging.critical("Failed to delete datafiles after trying to get new session. Exiting")
+    log_response(response)
+    raise RuntimeError
 
 #async def?
-def process_locations_file(locations_file, batch_size, host, session):
+def process_locations_file(locations_file, batch_size, host, session, auth):
     """Takes an open file of Datafile locations, iterates over them, getting the
     ID of each then deleting them in batches"""
 
@@ -101,6 +149,7 @@ def process_locations_file(locations_file, batch_size, host, session):
     # Get url and append ID to list (aiohttp?)
     # After batch loop (async gather?), call delete(Many) url
 
+    connection_pool = requests.Session()
     locations = iter(locations_file)
 
     while True:
@@ -114,7 +163,8 @@ def process_locations_file(locations_file, batch_size, host, session):
         deletes = list()
         #async for?
         for location in batch:
-            datafile_id = get_datafile_id(location, host, session)
+            datafile_id = get_datafile_id(location, host, session,
+                    connection_pool, auth)
             if datafile_id is not None:
                 deletes.append(datafile_id)
             # will this work OK in an async context? Maybe zip the batch
@@ -123,7 +173,7 @@ def process_locations_file(locations_file, batch_size, host, session):
             # correct position in the index
 
         if len(deletes) != 0:
-            delete_datafiles(deletes, host, session)
+            delete_datafiles(deletes, host, session, connection_pool, auth)
         else:
             logging.warning("No files to delete from the batch")
 
@@ -189,7 +239,8 @@ def main():
     logging.debug("Batch size is %s", str(args.batch_size))
     logging.debug("Host is %s", args.icat_host)
     logging.debug("Session is %s", session)
-    process_locations_file(locations_file, args.batch_size, args.icat_host, session)
+    process_locations_file(locations_file, args.batch_size, args.icat_host,
+            session, (args.mechanism, args.user, password))
 
     locations_file.close()
 
