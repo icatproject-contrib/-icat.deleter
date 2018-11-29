d1fd8f883b6ab9a4215168cba5902aea55c1271e
Stuart Pullinger <stuartpullinger@gmail.com>
2018-11-29 14:19:53 +0000

    Added Github URL to setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from distutils.core import setup
 setup(name='delete_datafiles',
         version='0.1',
         description= "Takes a file containing one file path per line and deletes the corresponding DataFile entities from ICAT",
+        url="https://github.com/icatproject-contrib/icat.deleter",
         author="Stuart Pullinger",
         author_email="stuart.pullinger@stfc.ac.uk",
         scripts=['delete_datafiles.py'],
